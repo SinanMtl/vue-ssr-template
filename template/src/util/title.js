1599912cff13ce645b2f298b6897302dc3b76fe4
Sinan Mutlu <sinanm17@gmail.com>
2017-07-30 02:52:09 +0300

    Title mixin edited

--- template/src/util/title.js
+++ template/src/util/title.js
@@ -11,7 +11,7 @@ const serverTitleMixin = {
   created () {
     const title = getTitle(this)
     if (title) {
-      this.$ssrContext.title = `${title}`
+      this.$ssrContext.title = `${title} | {{ name }}`
     }
   }
 }
@@ -20,7 +20,7 @@ const clientTitleMixin = {
   mounted () {
     const title = getTitle(this)
     if (title) {
-      document.title = `${title} {{ name }}`
+      document.title = `${title} | {{ name }}`
     }
   }
 }
